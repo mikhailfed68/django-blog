a035e14b11de31e7388945ed7dbb38f3d4ed8267
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-11-18 21:24:26 +0300

    Create main models for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Author(TimeStampedModel):
+    first_name = models.CharField(max_length=16)
+    surname = models.CharField(max_length=16)
+    alias = models.CharField(max_length=16, unique=True)
+    email = models.EmailField(unique=True)
+
+
+class Article(TimeStampedModel):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField('Name article', max_length=256)
+    body = models.TextField()
