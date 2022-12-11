203b3a2d62cc5ad406b3ef57850a1fb599eedb2c
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-12-11 18:44:04 +0300

    Improve templates with bootstrap

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
-from blog.models import Tag
+from blog.models import Tag, Article
 
 
 class TagFrom(forms.ModelForm):
     class Meta:
         model = Tag
         fields = ['name']
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        exclude = ['author']
