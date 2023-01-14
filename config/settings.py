a6ce5328cc3a26682b8b4d5d9e4fc95221c5e124
Michael Fedorov <mikhailfedorov1939@gmail.com>
2023-01-14 15:50:50 +0300

    Add a filter-django app

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'bootstrap5',
+    'django_filters',
 
     'users',
     'blog',
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include('blog.urls')),
     path('search/', include('search.urls')),
