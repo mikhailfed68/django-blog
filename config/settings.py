570242c2b71e2adeb90f4a63179295518ccff557
Michael Fedorov <mikhailfedorov1939@gmail.com>
2023-01-15 17:41:33 +0300

    Fix last update output for articles

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'django.contrib.humanize',
     'bootstrap5',
     'django_filters',
 
