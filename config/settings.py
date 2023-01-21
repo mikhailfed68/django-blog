8538af8068f7159f5cf714c98237ce6943cbdbf6
Michael Fedorov <mikhailfedorov1939@gmail.com>
2023-01-21 17:14:19 +0300

    Add Redis configuration

--- config/settings.py
+++ config/settings.py
@@ -87,6 +87,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': os.getenv('REDIS_LOCATION'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
