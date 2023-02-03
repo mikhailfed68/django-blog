0427e3f2a1186b312eb23b9cbb10826da6ada3fd
Michael Fedorov <mikhailfedorov1939@gmail.com>
2023-02-03 21:17:52 +0300

    Update project settings file

--- config/settings.py
+++ config/settings.py
@@ -10,17 +10,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = json.loads(os.getenv('DEBUG'))
+
 ALLOWED_HOSTS = [os.getenv('HOST')]
-
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -131,11 +125,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'assets',
 ]
+
+DISABLE_COLLECTSTATIC = 0
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
